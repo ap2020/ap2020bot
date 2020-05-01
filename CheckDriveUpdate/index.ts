b3098fd17732229af58c5f675df3b309547588b8
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-05-01 23:04:41 +0900

    CheckDriveUpdate: implement basic logic

--- CheckDriveUpdate/index.ts
+++ CheckDriveUpdate/index.ts
@@ -1,10 +1,13 @@
 import {AzureFunction, Context} from "@azure/functions"
-import {google, driveactivity_v2} from 'googleapis';
+import {google, driveactivity_v2, people_v1, drive_v3} from 'googleapis';
 import {getGoogleClient} from '../utils/google-client';
-import {ignoredActions} from './config';
+import {ignoredActions, japaneseTranslations, colors} from './config';
+import {stripIndent} from 'common-tags';
+import moment from 'moment-timezone';
+import {slack} from '../utils/slack';
 
-import {promises as fs} from 'fs';
-import path from 'path';
+// import {promises as fs} from 'fs';
+// import path from 'path';
 
 const main: AzureFunction = async (context: Context, myTimer: any): Promise<void> => {
     // await checkUpdate();
@@ -14,26 +17,26 @@ export default main;
 
 const fetchAllDriveActivities = async (
     driveActivity: driveactivity_v2.Driveactivity,
+    folderId: string,
     since: Date,
 ): Promise<driveactivity_v2.Schema$DriveActivity[]> => {
 
-    if (process.env.NODE_ENV === 'development') {
-        try {
-            return JSON.parse(await fs.readFile(path.join(__dirname, '..', 'tmp', 'drive-activity-api-cache.json'), {encoding: 'utf-8'}));
-        } catch (e) {
-            console.warn('Failed to load cache.\nFetching...')
-        }
-    }
+    // if (process.env.NODE_ENV === 'development') {
+    //     try {
+    //         return JSON.parse(await fs.readFile(path.join(__dirname, '..', 'tmp', 'drive-activity-api-cache.json'), {encoding: 'utf-8'}));
+    //     } catch (e) {
+    //         console.warn('Failed to load cache.\nFetching...')
+    //     }
+    // }
 
     // may take a while. 
-    const root_folder_id = process.env.GOOGLE_ROOT_FOLDER_ID;
     let activities: driveactivity_v2.Schema$DriveActivity[] = [];
     let response: driveactivity_v2.Schema$QueryDriveActivityResponse | null = null;
     
     do {
         response = (await driveActivity.activity.query({
             requestBody: {
-                ancestorName: `items/${root_folder_id}`,
+                ancestorName: `items/${folderId}`,
                 filter: `time > ${since.getTime()}`,
                 pageToken: response?.nextPageToken,
                 consolidationStrategy: { legacy: {} }
@@ -42,13 +45,13 @@ const fetchAllDriveActivities = async (
         activities = activities.concat(response.activities);
     } while (response.nextPageToken);
     
-    if (process.env.NODE_ENV === 'development') {
-        try {
-            await fs.writeFile(path.join(__dirname, '..', 'tmp', 'drive-activity-api-cache.json'), JSON.stringify(activities));
-        } catch (e) {
-            console.error('Error while caching API response', e);
-        }
-    }
+    // if (process.env.NODE_ENV === 'development') {
+    //     try {
+    //         await fs.writeFile(path.join(__dirname, '..', 'tmp', 'drive-activity-api-cache.json'), JSON.stringify(activities));
+    //     } catch (e) {
+    //         console.error('Error while caching API response', e);
+    //     }
+    // }
 
     return activities;
 };
@@ -67,21 +70,152 @@ const getTargetName = (target: driveactivity_v2.Schema$Target): string => {
     // Target is not union...
 };
 
+// TODO: use batchGet https://developers.google.com/people/api/rest/v1/people/getBatchGet
+const getPersonName = async (client: people_v1.People, resourceName: string): Promise<string> => {
+    const person: people_v1.Schema$Person = (await client.people.get({resourceName, personFields: 'names' })).data;
+    const name = person.names?.[0]?.displayName;
+    return name ?? resourceName; // TODO: a better approach?
+};
+
+const formatDate = (timestamp: string): string =>
+    moment(timestamp).tz('Asia/Tokyo').format('YYYY/MM/DD HH:mm:ss');
+
+const getDate = (activity: driveactivity_v2.Schema$DriveActivity): string => {
+    if (activity.timestamp) {
+        return formatDate(activity.timestamp);
+    } else if (activity.timeRange) {
+        return `${formatDate(activity.timeRange.startTime)} - ${formatDate(activity.timeRange.endTime)}`
+    }
+};
+
+const getDriveItemId = (target: driveactivity_v2.Schema$Target): string => {
+    let itemName: string;
+    if (target.driveItem) itemName = target.driveItem.name;
+    else if (target.drive) itemName = target.drive.name;
+    else if (target.fileComment) itemName = target.fileComment.parent.name;
+    // else {
+    //   const _exhaustiveCheck: never = target;
+    //   itemName = _exhaustiveCheck;
+    // }
+    return itemName.replace(/^items\//, '');
+};
+
+/**
+ * cache for getPath
+ * folderId => path
+ */
+const paths = new Map<string, string>();
+const getPath = async (client: drive_v3.Drive, rootFolderId: string, item: drive_v3.Schema$File): Promise<string> => {
+    if (paths.has(item.id)) {
+        return paths.get(item.id);
+    }
+    if (item.id == rootFolderId) {
+        return '/';
+    }
+    /**
+     * path: path to item
+     * valid: true if path has rootFolderId in ancestor
+     */
+    const rec = async (folder: drive_v3.Schema$File): Promise<{path: string | null; valid: boolean}> => {
+        // I want to use Promise.any...
+        if (!folder.parents) {
+            // folder is root of drive
+            return { path: null, valid: false };
+        }
+        const parentPaths = (await Promise.all(folder.parents.map(async parentId => {
+            if (paths.has(parentId)) {
+                return { path: paths.get(parentId)!, valid: true };
+            }
+            if (parentId == rootFolderId) {
+                return { path: '/', valid: true };
+            }
+            const parent = (await client.files.get({fileId: parentId, fields: 'id,name,parents,mimeType'})).data;
+            return rec(parent)
+        }))).filter(({valid}) => valid);
+        if (parentPaths.length > 0) {
+            const isFolder = folder.mimeType === 'application/vnd.google-apps.folder';
+            // folder has one or more parents that is child of rootFolderId
+            const path = `${parentPaths[0].path}${folder.name}${isFolder? '/': ''}`;
+            paths.set(folder.id, path);
+            return { path, valid: true };
+        } else {
+            return { path: null, valid: false };
+        }
+    };
+    const path = await rec(item);
+    return path.valid? path.path: item.name;
+};
+
+const getEmoji = (item: drive_v3.Schema$File): string => {
+    switch (item.mimeType) {
+        case 'application/vnd.google-apps.folder':
+            return ':file_folder:';
+        default:
+            return ':file:';
+    }
+};
+
 const checkUpdate = async (since: Date) => {
     const auth = getGoogleClient();
     const drive = google.drive({version: 'v3', auth});
-    //console.log(await drive.files.list({pageSize: 3}));
     const driveActivity = google.driveactivity({version: "v2", auth});
-    
+    const peopleAPI = google.people({version: 'v1', auth});
+    const rootFolderId = process.env.GOOGLE_ROOT_FOLDER_ID;
+    const drivelogId = process.env.SLACK_CHANNEL_DRIVE;
+
     const lastChecked = Date.now();
-    const activities = await fetchAllDriveActivities(driveActivity, since);
-    for (const activity of activities) {
+    const activities = await fetchAllDriveActivities(driveActivity, rootFolderId, since);
+    for (const activity of activities) { // TODO: use Promise.all
         if (!activity) continue;
         const actionName = getActionName(activity.primaryActionDetail);
         if (ignoredActions.includes(actionName)) {
             continue;
         }
-        console.log(activity);
+        const targets: driveactivity_v2.Schema$Target[] = activity.targets/*.filter(
+            target => !isIgnoredItem(getDriveItemfromTarget(target))
+        )*/; // TODO
+        if (targets.length === 0) continue;
+        const actorsText = (await Promise.all(
+            activity.actors.map(async actor => `${await getPersonName(peopleAPI, actor.user.knownUser.personName)}  さん`)
+        )).join(', ');
+        const text = stripIndent`
+            ${actorsText}が *${activity.targets.length}* 件のアイテムを *${japaneseTranslations[actionName]}* しました。
+            発生日時: ${getDate(activity)}
+        `;
+        let fileURL: string | undefined = undefined;
+        if (targets.length <= 20) {
+            // attachments
+            const attachments = await Promise.all(targets.map(async target => {
+              const item = (await drive.files.get({fileId: getDriveItemId(target), fields: 'id,parents,name,mimeType,webViewLink'})).data; // TODO: cache files.get?
+              return {
+                color: colors[actionName],
+                title: `${japaneseTranslations[actionName]}: ${getEmoji(item)} ${await getPath(drive, rootFolderId, item)}`,
+                text: '', // TODO: include details
+                title_link: item.webViewLink
+              };
+            }));
+            // TODO: this await might be bad for performance?
+            await slack.bot.chat.postMessage({
+                channel: drivelogId,
+                text, 
+                icon_emoji: ':google_drive:',
+                username: 'UpdateNotifier',
+                attachments: attachments,
+            });
+        } else {
+            // snippet
+            fileURL = ((await slack.bot.files.upload({
+                channels: [drivelogId].join(','),
+                content: (await Promise.all(targets.map(
+                    async target => {
+                        const item = (await drive.files.get({fileId: getDriveItemId(target), fields: 'id,name,parents,mimeType,webViewLink'})).data;
+                        return `${japaneseTranslations[actionName]}: ${getPath(drive, rootFolderId, item)} (${item.webViewLink})`
+                    },
+                    ))).join('\n'),
+                text,
+            })) as any).file.permalink;
+            // TODO: not checked!
+          }
     }
 }
 
