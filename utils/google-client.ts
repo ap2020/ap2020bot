4453e7f4283a5a99c0a33f8d97775e965f58caba
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-05-02 15:58:30 +0900

    utils: google credentials envvar

--- utils/google-client.ts
+++ utils/google-client.ts
@@ -3,9 +3,9 @@ import {google} from 'googleapis';
 export type OAuth2Client = InstanceType<typeof google.auth.OAuth2>;
 
 export const getGoogleClient = (): OAuth2Client => {
-    const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
-    const {client_secret, client_id, redirect_uris} = credentials.installed;  
-    const auth = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+    const client_id = process.env.GOOGLE_CLIENT_ID;
+    const client_secret = process.env.GOOGLE_CLIENT_SECRET;
+    const auth = new google.auth.OAuth2(client_id, client_secret);
     auth.setCredentials({refresh_token:process.env.GOOGLE_REFRESH_TOKEN});
     return auth;
 }
