1f92c6b1b10667786fffb5d28f7ed448d061d77b
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-05-03 16:37:37 +0900

    utils/slack: add slackTSToDate & test

--- utils/slack.ts
+++ utils/slack.ts
@@ -1,8 +1,12 @@
 import { WebClient } from '@slack/web-api';
 
 export const slack = {
-    bot: new WebClient(process.env.SLACK_TOKEN_BOT)
+    bot: new WebClient(process.env.SLACK_TOKEN_BOT),
+    admin: new WebClient(process.env.SLACK_TOKEN_ADMIN),
 };
 
 export const dateToSlackTS = (date: Date): string =>
     (date.getTime()/1000).toFixed(3);
+
+export const slackTSToDate = (ts: string): Date =>
+    new Date(Number(ts) * 1000)
