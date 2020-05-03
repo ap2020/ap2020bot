9e4fafd3ee9d5fe3cbb7914cc3c8a87cfb0e6b67
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-05-03 15:29:32 +0900

    utils: add slack-types

---
+++ utils/slack-types.ts
@@ -0,0 +1,31 @@
+import type {WebAPICallResult} from '@slack/web-api';
+
+export module Slack {
+    export interface Message {
+        type: string;
+        subtype?: string;
+        user: string;
+        text: string;
+        ts: string;
+        blocks?: unknown[];
+        attachments?: unknown[];
+        client_msg_id?: string;
+        team?: string;
+    }
+
+    export module Conversation {
+        export interface History extends WebAPICallResult {
+            ok: boolean;
+            response_metadata?:  {
+                next_cursor: string;
+            };
+            messages: Message[];
+            has_more: boolean;
+            pin_count: number;
+            is_limited?: boolean;
+            latest?: string;
+            channel_actions_ts?: unknown;
+            channel_actions_count?: number;
+        }
+    }
+}
