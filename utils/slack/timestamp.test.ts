417c9f84f80d88408f566d2bc791a9370344bd1e
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-05-16 20:43:08 +0900

    utils/slack/timestamp: Slack timestamp has 6 digits after .

--- utils/slack/timestamp.test.ts
+++ utils/slack/timestamp.test.ts
@@ -3,12 +3,12 @@ import {momentToSlackTS, slackTSToMoment} from './timestamp';
 
 describe('dateToSlackTS', () => {
     it('converts Date object to timestamp', () => {
-        expect(momentToSlackTS(moment(1234567890123).tz('Asia/Tokyo'))).toBe('1234567890.123');
+        expect(momentToSlackTS(moment(1234567890123).tz('Asia/Tokyo'))).toBe('1234567890.123000');
     });
 });
 
 describe('slackTSToDate', () => {
     it('converts timestamp to Date object', () => {
-        expect(slackTSToMoment('1234567890.123')).toEqual(moment(1234567890123).tz('Asia/Tokyo'));
+        expect(slackTSToMoment('1234567890.123000')).toEqual(moment(1234567890123).tz('Asia/Tokyo'));
     });
 })
--- utils/slack/timestamp.ts
+++ utils/slack/timestamp.ts
@@ -1,7 +1,7 @@
 import moment, { Moment } from 'moment-timezone';
 
 export const momentToSlackTS = (date: Moment): string =>
-    (date.valueOf() / 1000).toFixed(3);
+    (date.valueOf() / 1000).toFixed(6);
 
 export const slackTSToMoment = (ts: string): Moment =>
     moment(Number(ts) * 1000).tz('Asia/Tokyo');
