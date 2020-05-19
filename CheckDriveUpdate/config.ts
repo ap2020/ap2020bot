093099cf349cb9f06b695539dcc582cac11533fd
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-05-19 10:27:21 +0900

    CheckDriveUpdate: lint config.ts

--- CheckDriveUpdate/config.ts
+++ CheckDriveUpdate/config.ts
@@ -1,21 +1,21 @@
 export const ignoredActions = ['comment', 'dlpChange', 'reference'];
 export const colors = {
-  create: '#6dc651',
-  edit: '#f5bc41',
-  move: '#1f28c6',
-  rename: '#7337d3',
-  delete: '#ec565c',
-  restore: '#51c69b',
-  permissionChange: '#ec56a5',
-  settingsChange: '#ec56a5'
+    create: '#6dc651',
+    edit: '#f5bc41',
+    move: '#1f28c6',
+    rename: '#7337d3',
+    delete: '#ec565c',
+    restore: '#51c69b',
+    permissionChange: '#ec56a5',
+    settingsChange: '#ec56a5',
 };
 export const japaneseTranslations = {
-  create: '追加',
-  edit: '更新',
-  move: '移動',
-  rename: '名称変更',
-  delete: '削除',
-  restore: '復元',
-  permissionChange: '権限変更',
-  settingsChange: '設定変更'
-}
+    create: '追加',
+    edit: '更新',
+    move: '移動',
+    rename: '名称変更',
+    delete: '削除',
+    restore: '復元',
+    permissionChange: '権限変更',
+    settingsChange: '設定変更',
+};
