9cfacc4b0bc98716917131431f079133c91cb724
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-05-19 12:35:42 +0900

    CheckDriveUpdate: lint drive-api

--- CheckDriveUpdate/drive-api.ts
+++ CheckDriveUpdate/drive-api.ts
@@ -1,8 +1,6 @@
-import {drive_v3} from 'googleapis';
-import {cacheCalls} from '../utils/utils';
-import {rootFolderId} from './lib';
-
-const driveItems = new Map<string, Promise<DriveItem>>()
+import { drive_v3 } from 'googleapis';
+import { cacheCalls } from '../utils/utils';
+import { rootFolderId } from './lib';
 
 // TODO: drive-activity-apiと名前が衝突している
 // DriveItemはDrive Activityの概念なので↓を改名すべきか？
@@ -20,58 +18,80 @@ export class DriveItem {
     }
 }
 
-export const fetchDriveItem = cacheCalls(async (client: drive_v3.Drive, id: string): Promise<DriveItem> =>
-    new DriveItem(client, (await client.files.get({fileId: id, fields: 'id,name,parents,mimeType,webViewLink,permissions'})).data)
-, (c, id) => id);
+export const fetchDriveItem =
+    cacheCalls(
+        async (client: drive_v3.Drive, id: string): Promise<DriveItem> =>
+            new DriveItem(client, (await client.files.get({
+                fileId: id,
+                fields: 'id,name,parents,mimeType,webViewLink,permissions',
+            })).data),
+        (c, id) => id,
+    );
 
 /**
  * cache for getPath
  * folderId => path
  */
-const paths: Map<string, Promise<{path: string | null, valid: boolean}>> = new Map([[rootFolderId, Promise.resolve({path: '/', valid: true})]]);
+const paths: Map<string, Promise<{path: string | null; valid: boolean}>> =
+    new Map([[rootFolderId, Promise.resolve({ path: '/', valid: true })]]);
 export const getPath = async (client: drive_v3.Drive, item: DriveItem): Promise<string> => {
     /**
      * path: path to item
      * valid: true if path has rootFolderId in ancestor
      */
-    const rec = cacheCalls(async (client: drive_v3.Drive, folderId: string): Promise<{path: string | null; valid: boolean}> => {
-        const folder = await fetchDriveItem(client, folderId);
+    const rec = cacheCalls(async (drive: drive_v3.Drive, folderId: string): Promise<{path: string | null; valid: boolean}> => {
+        const folder = await fetchDriveItem(drive, folderId);
         if (!folder.content.parents) {
             // folder is root of drive
             return { path: null, valid: false };
         }
         // I want to use Promise.any...
         const parentPaths = (
-            (await Promise.all(folder.content.parents.map(async parentId => rec(client, parentId))))
-            .filter(({valid}) => valid).map(({path}) => path)
+            (await Promise.all(folder.content.parents.map(async parentId => rec(drive, parentId))))
+                .filter(({ valid }) => valid).map(({ path }) => path)
         );
         if (parentPaths.length > 0) {
             const isFolder = folder.content.mimeType === 'application/vnd.google-apps.folder';
             // folder has one or more parents that is child of rootFolderId
-            return { path: `${parentPaths[0]}${folder.content.name}${isFolder? '/': ''}`, valid: true };
-        } else {
-            // rootFolderId is not ancestor of folderId...
-            return { path: null, valid: false };
+            return {
+                path: `${parentPaths[0]}${folder.content.name}${isFolder ? '/' : ''}`,
+                valid: true,
+            };
         }
+        // rootFolderId is not ancestor of folderId...
+        else {
+            return {
+                path: null,
+                valid: false,
+            }
+        };
     }, (c, id) => id, paths);
     const path = await rec(client, item.content.id);
-    return path.valid? path.path: item.content.name;
+    return path.valid ? path.path : item.content.name;
 };
 
-export const isIgnored = cacheCalls(async (client: drive_v3.Drive, itemId: string): Promise<boolean> => {
-    const item = await fetchDriveItem(client, itemId);
-    if(!item.content.parents) {
-        // root of drive
-        return false;
-    }
-    if (item.content.parents.length === 0) {
-        // we don't need to ignore this
-        return false;
-    }
-    const isParentsIgnored = await Promise.all(item.content.parents.map(async parentId => isIgnored(client, parentId)));
-    // 全ての親がignoredならtrue
-    return isParentsIgnored.every((x) => x);
-}, (c, id) => id, new Map([
-    ...((process.env.GOOGLE_DRIVE_IGNORED_IDS ?? '').split(',').map(s => [s.trim(), Promise.resolve(true)] as [string, Promise<boolean>])),
-    [rootFolderId, Promise.resolve(false)],
-]));
+export const isIgnored = cacheCalls(
+    async (client: drive_v3.Drive, itemId: string): Promise<boolean> => {
+        const item = await fetchDriveItem(client, itemId);
+        if (!item.content.parents) {
+            // root of drive
+            return false;
+        }
+        if (item.content.parents.length === 0) {
+            // we don't need to ignore this
+            return false;
+        }
+        const isParentsIgnored = await Promise.all(
+            item.content.parents.map(async parentId => isIgnored(client, parentId)),
+        );
+        // 全ての親がignoredならtrue
+        return isParentsIgnored.every((x) => x);
+    },
+    (c, id) => id,
+    new Map([
+        ...(process.env.GOOGLE_DRIVE_IGNORED_IDS ?? '')
+            .split(',')
+            .map(s => [s.trim(), Promise.resolve(true)] as const),
+        [rootFolderId, Promise.resolve(false)],
+    ]),
+);
