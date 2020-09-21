1fe6fbaa5252e1ab2c2f5532ec216c6162d9d784
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-09-21 11:04:33 +0900

    eslint fix

--- aws/serverless.ts
+++ aws/serverless.ts
@@ -1,44 +1,45 @@
 import type { Serverless } from 'serverless/aws';
 
 const serverlessConfiguration: Serverless = {
-  service: {
-    name: 'aws',
+    service: {
+        name: 'aws',
     // app and org for use with dashboard.serverless.com
     // app: your-app-name,
     // org: your-org-name,
-  },
-  frameworkVersion: '2',
-  custom: {
-    webpack: {
-      webpackConfig: './webpack.config.js',
-      includeModules: true
-    }
-  },
-  // Add the serverless-webpack plugin
-  plugins: ['serverless-webpack'],
-  provider: {
-    name: 'aws',
-    runtime: 'nodejs12.x',
-    apiGateway: {
-      minimumCompressionSize: 1024,
     },
-    environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    frameworkVersion: '2',
+    custom: {
+        webpack: {
+            webpackConfig: './webpack.config.js',
+            includeModules: true,
+        },
     },
-  },
-  functions: {
-    hello: {
-      handler: 'handler.hello',
-      events: [
-        {
-          http: {
-            method: 'get',
-            path: 'hello',
-          }
-        }
-      ]
-    }
-  }
-}
+    // Add the serverless-webpack plugin
+    plugins: ['serverless-webpack'],
+    provider: {
+        name: 'aws',
+        runtime: 'nodejs12.x',
+        apiGateway: {
+            minimumCompressionSize: 1024,
+        },
+        environment: {
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+        },
+    },
+    functions: {
+        hello: {
+            handler: 'handler.hello',
+            events: [
+                {
+                    http: {
+                        method: 'get',
+                        path: 'hello',
+                    },
+                },
+            ],
+        },
+    },
+};
 
 module.exports = serverlessConfiguration;
