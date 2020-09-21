a51dbd9eaa99092ecde8e1b9610509cfd29c4b22
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-09-21 19:05:45 +0900

    setup jest

---
+++ aws/jest.config.js
@@ -0,0 +1,28 @@
+const ignorePatterns = [
+    '/node_modules/',
+    '<rootDir>/.webpack/',
+    '<rootDir>/.dynamodb/',
+    '<rootDir>/.vscode/',
+    '/coverage/',
+];
+
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+    testPathIgnorePatterns: [
+        ...ignorePatterns,
+    ],
+    collectCoverageFrom: [
+        '**/*.{js,ts}',
+        '!**/*.test.{js,ts}'
+    ],
+    coveragePathIgnorePatterns: [
+        ...ignorePatterns,
+        '<rootDir>/jest.config.js',
+        '<rootDir>/.env.js',
+    ],
+    setupFiles: [
+        './test-utils/setup.ts',
+    ],
+};
---
+++ aws/test-utils/setup.ts
@@ -0,0 +1,3 @@
+// settings not to affect production servers
+process.env.IS_LOCAL = '1';
+process.env.IS_OFFLINE = '1';
