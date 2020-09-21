c141a49c74ba4251686a8a196e3941b1b1bd5a09
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-09-21 11:11:03 +0900

    set tab size to 2 in serverless.ts

--- aws/serverless.ts
+++ aws/serverless.ts
@@ -1,45 +1,46 @@
+/* eslint indent: ["warn", 2] */
 import type { Serverless } from 'serverless/aws';
 
 const serverlessConfiguration: Serverless = {
-    service: {
-        name: 'aws',
+  service: {
+    name: 'aws',
     // app and org for use with dashboard.serverless.com
     // app: your-app-name,
     // org: your-org-name,
+  },
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true,
     },
-    frameworkVersion: '2',
-    custom: {
-        webpack: {
-            webpackConfig: './webpack.config.js',
-            includeModules: true,
-        },
+  },
+  // Add the serverless-webpack plugin
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
     },
-    // Add the serverless-webpack plugin
-    plugins: ['serverless-webpack'],
-    provider: {
-        name: 'aws',
-        runtime: 'nodejs12.x',
-        apiGateway: {
-            minimumCompressionSize: 1024,
-        },
-        environment: {
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-        },
+    environment: {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
     },
-    functions: {
-        hello: {
-            handler: 'handler.hello',
-            events: [
-                {
-                    http: {
-                        method: 'get',
-                        path: 'hello',
-                    },
-                },
-            ],
+  },
+  functions: {
+    hello: {
+      handler: 'handler.hello',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'hello',
+          },
         },
+      ],
     },
+  },
 };
 
 module.exports = serverlessConfiguration;
