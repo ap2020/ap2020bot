5449e590e7824ed91980a9184e6f39d4faf49fac
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-09-21 17:09:21 +0900

    watch-portal: refactoring

--- aws/handler.ts
+++ aws/handler.ts
@@ -1,30 +1,37 @@
 import { ScheduledHandler } from 'aws-lambda';
-import axios, { AxiosRequestConfig } from 'axios';
+import axios from 'axios';
 import Parser from 'rss-parser';
 import 'source-map-support/register';
 
-const parser = new Parser();
-
-const url = 'https://info.t.u-tokyo.ac.jp/rss/index.xml';
-
-const config: AxiosRequestConfig = {
-    proxy: {
-        host: process.env.UTOKYO_PROXY_HOST,
-        port: Number(process.env.UTOKYO_PROXY_PORT),
-        auth: {
-            username: process.env.UTOKYO_PROXY_USERNAME,
-            password: process.env.UTOKYO_PROXY_PASSWORD,
+const fetchRSS = async (): Promise<string> => {
+    const { data } = await axios.get(
+        'https://info.t.u-tokyo.ac.jp/rss/index.xml',
+        {
+            proxy: {
+                host: process.env.UTOKYO_PROXY_HOST,
+                port: Number(process.env.UTOKYO_PROXY_PORT),
+                auth: {
+                    username: process.env.UTOKYO_PROXY_USERNAME,
+                    password: process.env.UTOKYO_PROXY_PASSWORD,
+                },
+            },
+            headers: {
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                'User-Agent': process.env.USER_AGENT,
+            },
         },
-    },
-    headers: {
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        'User-Agent': process.env.USER_AGENT,
-    },
+    );
+    return data;
 };
 
-export const hello: ScheduledHandler = async (event, _context) => {
-    const { data } = (await axios.get(url, config));
+const extractItems = async (data: string): Promise<Parser.Item[]> => {
+    const parser = new Parser();
     const rss = await parser.parseString(data);
-    console.log(rss.title);
-    console.log(rss.items);
+    return rss.items;
+};
+
+export const hello: ScheduledHandler = async () => {
+    const data = await fetchRSS();
+    const items = await extractItems(data);
+    console.log(items);
 };
