0c8bc2371caa28483e6951b8b9f775e473bd8782
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-09-23 05:36:59 +0900

    lib/envvar: forgot to cache envvars

--- aws/lib/envvar/prod.ts
+++ aws/lib/envvar/prod.ts
@@ -6,15 +6,21 @@ export class EnvVarProd implements EnvVar {
     ssm = new AWS.SSM();
     cache = new Map<string, Promise<string>>();
 
-    async get(key: string): Promise<string> {
-        if (this.cache.has(key)) {
-            return this.cache.get(key);
-        }
+    private async fetch(key: string): Promise<string> {
         const res = await this.ssm.getParameter({
             Name: `/ap2020bot/${key}`,
             WithDecryption: true,
         }).promise();
         const value = res.Parameter.Value;
         return value;
+    }
+
+    async get(key: string): Promise<string> {
+        if (this.cache.has(key)) {
+            return this.cache.get(key);
+        }
+        const promise: Promise<string> = this.fetch(key);
+        this.cache.set(key, promise);
+        return promise;
     }
 }
