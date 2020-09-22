4b75f8290ff0f566161d6c01339735e286e9b60e
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-09-23 01:05:32 +0900

    lib/dynamodb: use STAGE to determine which config to use

--- aws/lib/dynamodb.ts
+++ aws/lib/dynamodb.ts
@@ -1,10 +1,10 @@
 import AWS from "aws-sdk";
 
 export const db = new AWS.DynamoDB.DocumentClient(
-    process.env.IS_OFFLINE || process.env.IS_LOCAL ?
+    process.env.STAGE === 'prod' ?
+        {} :
         {
             region: 'localhost',
             endpoint: 'http://localhost:8000',
-        } :
-        {},
+        },
 );
