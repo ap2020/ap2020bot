07e56e3cbfe9cf4b10121baab877680472a1ca4f
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-09-23 01:04:16 +0900

    lib/envvar: implement local mock

---
+++ aws/lib/envvar/local.ts
@@ -0,0 +1,19 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import { IEnvVar } from './base';
+
+export class EnvVarLocal implements IEnvVar {
+    envvars: Map<string, string> | null
+
+    private async loadEnv() {
+        const text = await fs.readFile(path.join(__dirname, '..', '..', '.env.local.json'), { encoding: 'utf8' });
+        this.envvars = new Map(Object.entries(JSON.parse(text)).map(([k, v]) => [k, v.toString()]));
+    }
+
+    async get(key: string) {
+        if (this.envvars === null) {
+            await this.loadEnv();
+        }
+        return this.envvars.get(key);
+    }
+}
