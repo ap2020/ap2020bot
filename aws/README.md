e981dba4cbb8173b8a7787461a593a3a8ba70199
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-10-02 00:51:25 +0900

    add doc

--- aws/README.md
+++ aws/README.md
@@ -6,6 +6,11 @@
 | [watch-portal](src/watch-portal) | 工学部ポータルサイト更新通知 |
 | [slash/invite-all](src/slash/invite-all) | `/invite-all`: チャンネルにワークスペース全員を招待する Slash Command |
 
+## `ifdef-loader`
+C プリプロセッサの `#if` みたいなことができます。
+
+TypeScript は `ifdef-loader` のディレクティブを無視して解析を行うため，`ifdef-loader` を不必要に使うと TypeScript の静的解析の強みが失われます。`ifdef-loader` はどうしても静的に削除しなければならないコードに限って使ってください。
+
 ## for Beginners
 既存の bot のソースコードを読むと何をすればいいかイメージが湧くかもしれません。
 自分が作ろうとしているものとやることが近い bot のソースコードを読むのがおすすめです。
--- aws/src/lib/envvar/index.ts
+++ aws/src/lib/envvar/index.ts
@@ -4,7 +4,11 @@ import { envvarProd } from './prod';
 import { envvarTest } from './test';
 import { envvarLocal } from './local';
 /// #endif
-// do not bundle local envvar module because this depends on .env.local.json
+// Do not bundle local envvar module because this depends on file .env.local.json,
+// which does not exist on CI.
+// This also prevents deploying bundle including contents of .env.local.json
+// from local environment.
+
 
 export const envvar: EnvVar = (() => {
     switch (process.env.STAGE) {
