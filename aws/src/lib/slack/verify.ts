c2a9f2d00c07406ae704ac6843ab33ee0daad604
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-12-17 04:53:16 +0900

    :sparkles: log slack signature verification error

--- aws/src/lib/slack/verify.ts
+++ aws/src/lib/slack/verify.ts
@@ -3,16 +3,20 @@ import timingSafeCompare from 'tsscmp';
 import { APIGatewayProxyEventV2 } from 'aws-lambda';
 import { envvar } from '@/lib/envvar';
 
+const logInvalidRequest = (req: APIGatewayProxyEventV2, message: string) => {
+    console.error('Slack signature verification failed.', message, req.requestContext.http.sourceIp, req.requestContext.http.userAgent);
+}
+
 export const verify = async (req: APIGatewayProxyEventV2): Promise<boolean> => {
     const version = 'v0';
     const actual = req.headers['X-Slack-Signature'];
     if (actual === undefined) {
-        // TODO: logging
+        logInvalidRequest(req, 'X-Slack-Signature not set.');
         return false;
     }
     const timestamp = req.headers['X-Slack-Request-Timestamp'];
     if (timestamp === undefined) {
-        // TODO: logging
+        logInvalidRequest(req, 'X-Slack-Request-Timestamp not set.');
         return false;
     }
     // TODO: time check
@@ -20,6 +24,9 @@ export const verify = async (req: APIGatewayProxyEventV2): Promise<boolean> => {
     hmac.update(`${version}:${timestamp}:${req.body}`);
     const expected = `${version}=${hmac.digest('hex')}`;
 
-    // TODO: logging
-    return timingSafeCompare(expected, actual);
+    const ok = timingSafeCompare(expected, actual);
+    if (!ok) {
+        logInvalidRequest(req, 'Signature did not match.');s
+    }
+    return ok;
 };
