4d65027f6bf72f539c89f0faee219fda8eaeb035
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-12-17 17:15:01 +0900

    :recycle: lint

--- aws/src/slack/events/attribute.ts
+++ aws/src/slack/events/attribute.ts
@@ -1,12 +1,12 @@
-import { EventPayload } from "@/lib/slack/events/types";
-import { MessageAttributeMap, MessageAttributeValue } from "aws-sdk/clients/sns";
+import { EventPayload } from '@/lib/slack/events/types';
+import type { MessageAttributeMap, MessageAttributeValue } from 'aws-sdk/clients/sns';
 
-const toAttr = (str: string): MessageAttributeValue => {
-  return {
+const toAttr = (str: string): MessageAttributeValue => ({
+    /* eslint-disable @typescript-eslint/naming-convention */
     DataType: 'String',
     StringValue: str,
-  }
-}
+    /* eslint-enable @typescript-eslint/naming-convention */
+});
 
 // type ExtractEventSpecificAttributeFunc<Kind extends SlackEvent['type']> = (event: Extract<SlackEvent, {type: Kind}>) => MessageAttributeMap;
 
@@ -15,20 +15,21 @@ const toAttr = (str: string): MessageAttributeValue => {
 // }
 
 const extractEventSpecificAttribute /*:  {[key in SlackEvent['type']]?: ExtractEventSpecificAttributeFunc<key>} */ = {
-  // 'message': (event) => extractAttributeFromKeys(event, ['channel', 'user', 'text'])
+    // 'message': (event) => extractAttributeFromKeys(event, ['channel', 'user', 'text'])
 } as const;
 
 export const extractAttribute = (payload: EventPayload): MessageAttributeMap => {
     const commonAttribute = {
-      team_id: toAttr(payload.team_id),
-      event_name: toAttr(payload.event.type),
-    }
+        /* eslint-disable @typescript-eslint/naming-convention */
+        team_id: toAttr(payload.team_id),
+        event_name: toAttr(payload.event.type),
+        /* eslint-enable @typescript-eslint/naming-convention */
+    };
     if (payload.event.type in extractEventSpecificAttribute) {
-      return {
-        ...commonAttribute,
-        ...extractEventSpecificAttribute[payload.event.type](payload.event),
-      }
-    } else {
-      return commonAttribute;
+        return {
+            ...commonAttribute,
+            ...extractEventSpecificAttribute[payload.event.type](payload.event),
+        };
     }
-}
+    return commonAttribute;
+};
--- aws/src/slack/events/index.ts
+++ aws/src/slack/events/index.ts
@@ -1,28 +1,42 @@
 import { APIGatewayProxyHandlerV2 } from 'aws-lambda';
 import { verify } from '@/lib/slack/verify';
-import { EventPayload } from '../../lib/slack/events/types';
-import { extractAttribute } from './attribute';
 import { getSlackEventTopicARN } from '@/lib/slack/events';
 import { sns } from '@/lib/sns';
+import { EventPayload } from '../../lib/slack/events/types';
+import { extractAttribute } from './attribute';
+
+const main = async (payload: EventPayload) => {
+    const message = payload;
+    const messageAttributes = extractAttribute(payload);
+    // TODO: exclude events by bots
+    await sns.publish({
+        /* eslint-disable @typescript-eslint/naming-convention */
+        TopicArn: await getSlackEventTopicARN(),
+        Message: JSON.stringify(message),
+        MessageAttributes: messageAttributes,
+        // MessageDeduplicationId: payload.event_id, // type definition of sns.publish is incomplete…
+        /* eslint-enable @typescript-eslint/naming-convention */
+    }).promise();
+};
 
 // TODO: preload envvars
 export const handler: APIGatewayProxyHandlerV2 = async (request) => {
     if (!(await verify(request))) {
-        console.error('Slack verification failed.', request)
+        console.error('Slack verification failed.', request);
         return {
             statusCode: 400,
             body: 'invalid request',
         };
     }
-    
+
     // verifyをしたので、payload は EventPayload の値であることが保証される
     const payload = JSON.parse(request.body);
 
     switch (payload.type) {
-        case "url_verification":
+        case 'url_verification':
             console.log('Challenge from Slack');
             return payload.challenge;
-        case "event_callback":
+        case 'event_callback':
             await main(payload);
             break;
         default:
@@ -32,17 +46,5 @@ export const handler: APIGatewayProxyHandlerV2 = async (request) => {
     return {
         statusCode: 200,
         body: '',
-    };;
+    };
 };
-
-const main = async (payload: EventPayload) => {
-    const message = payload;
-    const messageAttributes = extractAttribute(payload);
-    // TODO: exclude events by bots
-    await sns.publish({
-        TopicArn: await getSlackEventTopicARN(),
-        Message: JSON.stringify(message),
-        MessageAttributes: messageAttributes,
-        // MessageDeduplicationId: payload.event_id, // type definition of sns.publish is incomplete…
-    }).promise()
-}
