65783233d6add655b538e096a95e7732ad8e5ea8
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-03-03 02:51:43 +0900

    :bug: lib/slack/verify: handle HTTP header case insensitively

--- aws/src/lib/slack/verify.ts
+++ aws/src/lib/slack/verify.ts
@@ -3,6 +3,7 @@ import timingSafeCompare from 'tsscmp';
 import { APIGatewayProxyEventV2 } from 'aws-lambda';
 import { envvar } from '@/lib/envvar';
 import { DateTime, Duration } from 'luxon';
+import { CaseInsensitiveMap } from '../case-insensitive-map';
 
 const logInvalidRequest = (req: APIGatewayProxyEventV2, message: string) => {
     console.error('Slack signature verification failed.', message, req.requestContext.http.sourceIp, req.requestContext.http.userAgent);
@@ -11,12 +12,14 @@ const logInvalidRequest = (req: APIGatewayProxyEventV2, message: string) => {
 // TODO: use verify function of @slack/events-api ?
 export const verify = async (req: APIGatewayProxyEventV2): Promise<boolean> => {
     const version = 'v0';
-    const actual = req.headers['X-Slack-Signature'];
+    const headers = CaseInsensitiveMap.fromObject(req.headers);
+
+    const actual = headers.get('X-Slack-Signature');
     if (actual === undefined) {
         logInvalidRequest(req, 'X-Slack-Signature not set.');
         return false;
     }
-    const timestamp = req.headers['X-Slack-Request-Timestamp'];
+    const timestamp = headers.get('X-Slack-Request-Timestamp');
     if (timestamp === undefined) {
         logInvalidRequest(req, 'X-Slack-Request-Timestamp not set.');
         return false;
