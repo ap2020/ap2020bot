b77d9e3de270edb7c49eca969eaa632cdd411b93
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-03-03 03:04:51 +0900

    :recycle: aws/lib/case-insentive-map: lint

--- aws/src/lib/case-insensitive-map.ts
+++ aws/src/lib/case-insensitive-map.ts
@@ -4,20 +4,19 @@ export class CaseInsensitiveMap<Value> {
   constructor(map: Map<string, Value>) {
     this.loweredMap = new Map(
       [...map.entries()].map(([key, value]) => [key.toLowerCase(), value])
-    )
+    );
   }
 
-
   get(key: string): Value | undefined {
-    return this.loweredMap.get(key.toLowerCase())
+    return this.loweredMap.get(key.toLowerCase());
   }
 
   has(key: string): boolean {
-    return this.loweredMap.has(key.toLowerCase())
+    return this.loweredMap.has(key.toLowerCase());
   }
 
   set(key: string, value: Value) {
-    this.loweredMap.set(key.toLowerCase(), value)
+    this.loweredMap.set(key.toLowerCase(), value);
   }
 
   static fromObject<Value>(obj: {[key: string]: Value}): CaseInsensitiveMap<Value> {
