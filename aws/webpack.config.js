cca20ee9caa89783bb091002887058915ccec60e
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-03-23 17:19:21 +0900

    :wrench: aws/webpack: fix config due to upgrade

--- aws/webpack.config.js
+++ aws/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (async () => ({
   context: __dirname,
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   entry: slsw.lib.entries,
-  devtool: slsw.lib.webpack.isLocal ? 'cheap-module-eval-source-map' : 'source-map',
+  devtool: slsw.lib.webpack.isLocal ? 'eval-cheap-module-source-map' : 'source-map',
   resolve: {
     extensions: ['.mjs', '.json', '.ts'],
     symlinks: false,
