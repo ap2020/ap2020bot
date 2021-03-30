8afb2aaceb20585b5fb5296b091471db176352f9
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-03-30 18:40:08 +0900

    :recycle: aws: lint

--- aws/src/lib/google/index.ts
+++ aws/src/lib/google/index.ts
@@ -4,13 +4,15 @@ import { envvar } from '../envvar';
 export const googleAuth = async (): Promise<InstanceType<typeof google.auth.GoogleAuth>> => {
   const auth = new google.auth.GoogleAuth({
     credentials: {
+      /* eslint-disable @typescript-eslint/naming-convention */
       client_email: await envvar.get('google/auth/client-email'),
       private_key: await envvar.get('google/auth/private-key'),
+      /* eslint-enable @typescript-eslint/naming-convention */
     },
     scopes: [
       'https://www.googleapis.com/auth/drive',
       'https://www.googleapis.com/auth/drive.activity.readonly',
-    ]
+    ],
   });
   return auth;
 };
