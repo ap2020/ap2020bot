3dd236e84bfb76429997eb39e0caf00fa0b0d5db
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-03-30 13:14:47 +0900

    :sparkles: aws/.../dynamodb/default: implement client for default table

---
+++ aws/src/lib/aws/dynamodb/default.ts
@@ -0,0 +1,44 @@
+import type { Option } from 'ts-results';
+import { None, Some } from 'ts-results';
+import { resourcePrefix } from '../utils';
+import { dynamodb } from './clients';
+
+export type DefaultTableContent = {
+  'watch-portal': { url: string[] };
+};
+
+export type DefaultTableKey = keyof DefaultTableContent;
+
+export type DefaultTableContentWithKey<Key extends DefaultTableKey> = (
+  & DefaultTableContent[Key]
+  & { key: Key }
+);
+
+const tableName = `${resourcePrefix}default`;
+export const defaultTable = {
+  async get<Key extends DefaultTableKey>(key: Key): Promise<Option<DefaultTableContentWithKey<Key>>> {
+    const result = await dynamodb.get({
+      /* eslint-disable @typescript-eslint/naming-convention */
+      TableName: tableName,
+      Key: {
+        key,
+      },
+      /* eslint-enable @typescript-eslint/naming-convention */
+    }).promise();
+    return result.Item === undefined ?
+      None :
+      Some(result.Item as DefaultTableContentWithKey<Key>);
+  },
+
+  async set<Key extends DefaultTableKey>(key: Key, value: DefaultTableContent[Key]): Promise<void> {
+    await dynamodb.put({
+      /* eslint-disable @typescript-eslint/naming-convention */
+      TableName: tableName,
+      Item: {
+        key,
+        ...value,
+      },
+      /* eslint-enable @typescript-eslint/naming-convention */
+    }).promise();
+  },
+};
