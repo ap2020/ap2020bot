8868aebed592d6a3fd898d7cbbd3a473d72c93f6
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-04-03 23:17:25 +0900

    :recycle: lint

--- aws/src/lib/slack/client.ts
+++ aws/src/lib/slack/client.ts
@@ -4,6 +4,6 @@ import { envvar } from '../envvar';
 export const slack = {
   // TODO: 即時実行をやめる
   bot: (async () => (new WebClient(await envvar.get('slack/token/bot'))))(),
-  user: async () => (new WebClient(await envvar.get('slack/token/user'))),
-  admin: async () => (new WebClient(await envvar.get('slack/token/admin'))),
+  user: async (): Promise<WebClient> => (new WebClient(await envvar.get('slack/token/user'))),
+  admin: async (): Promise<WebClient> => (new WebClient(await envvar.get('slack/token/admin'))),
 };
